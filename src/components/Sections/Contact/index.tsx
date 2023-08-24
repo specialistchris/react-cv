bfda1c9813b22461d8dfb1cefc3b15ea8571cec4
specialistchris <specialistchris@gmail.com>
2023-08-24 17:44:41 +0200

    link to gallery

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -1,7 +1,6 @@
 import {DevicePhoneMobileIcon, EnvelopeIcon, MapPinIcon} from '@heroicons/react/24/outline';
 import classNames from 'classnames';
 import {FC, memo} from 'react';
-import { BrowserRoute, Routes, Route } from 'react-router-dom';
 
 import {contact, SectionId} from '../../../data/data';
 import {ContactType, ContactValue} from '../../../data/dataDef';
