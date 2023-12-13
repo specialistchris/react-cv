d16609f2c5d09bdb33bc2f2b4f917953ed49de2f
specialistchris <specialistchris@gmail.com>
2023-12-13 23:16:55 +0100

    testimonials -> quotes

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -62,7 +62,7 @@ export const SectionId = {
   Resume: 'resume',
   Skills: 'skills',
   Stats: 'stats',
-  Testimonials: 'testimonials',
+  Testimonials: 'quotes',
 } as const;
 
 export type SectionId = typeof SectionId[keyof typeof SectionId];
