5b07bc944023915b2c1d3cee24bf7725f21869cb
specialistchris <specialistchris@gmail.com>
2024-04-15 09:16:31 +0200

    my flag icon fix

--- src/components/Icon/NZFlagIcon.tsx
+++ src/components/Icon/NZFlagIcon.tsx
@@ -4,14 +4,9 @@ import Icon, {IconProps} from './Icon';
 
 const NZFlagIcon: FC<IconProps> = memo(props => (
   <Icon {...props}>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 128">
-    <path fill="#00247d" d="M0 0h85.33v42.667H0z"/>
-    <path fill="#ffcc00" d="M0 85.333h85.33V128H0z"/>
-    <path fill="#ffcc00" d="M170.667 85.333h85.333V128H170.667z"/>
-    <path fill="#00247d" d="M170.667 0h85.333v42.667H170.667z"/>
-    <path fill="#cc142b" d="M0 42.667h256v42.666H0z"/>
-    </svg>
-    
+      <path
+      d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5"
+      fill="currentColor"></path>
   </Icon>
 ));
 
