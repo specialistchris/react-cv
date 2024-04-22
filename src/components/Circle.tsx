cb82f1a548c7d0b5601164ee08f5a20eac97cc0e
specialistchris <specialistchris@gmail.com>
2024-04-22 17:32:55 +0200

    add circle

---
+++ src/components/Circle.tsx
@@ -0,0 +1,39 @@
+interface CircleProps {
+    diameter: number;
+    background: string;
+    child: any;
+    border?: string;
+    emptyColour?: string;
+    fillRatio?: 0 | 25 | 50 | 75 | 100;
+}
+
+export default function Circle({diameter, background, child, border, fillRatio = 100, emptyColour}: CircleProps) {
+
+    let bg: any;
+    if (fillRatio === 100) {
+        bg= {background};
+
+    } else if (fillRatio === 0) {
+        bg = {background: emptyColour};
+    } else if (fillRatio === 25) {
+        bg = {backgroundImage: `linear-gradient(00deg, ${emptyColour} 50%, transparent 50%), linear-gradient(90deg,${background} 50%, transparent 50%`};
+    } else if (fillRatio === 50) {
+        bg = {backgroundImage: `linear-gradient(-90deg, ${emptyColour} 50%, transparent 50%), linear-gradient(90deg,${background} 50%, transparent 50%`};
+    } else if (fillRatio === 75) {
+        bg = {backgroundImage: `linear-gradient(180deg, transparent 50%, ${background} 50%), linear-gradient(90deg,${background} 50%, transparent 50%`};
+    }
+
+    const style = {
+        display: "flex¨",
+        width: diameter,
+        height: diameter,
+        borderRadius: "50%",
+        alignItems: "cen^ter",
+        justifyContent: "center",
+        border,
+        ...bg
+    }
+
+    return <div style={style}>{child}</div>
+
+}
