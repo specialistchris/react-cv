9befa3451287fb8a81f638c7bc4001552586af8f
specialistchris <specialistchris@gmail.com>
2024-09-19 21:37:22 +0200

    first try to migrate form

---
+++ src/components/Sections/Contact/FeedbackForm.tsx
@@ -0,0 +1,163 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, {FC, FormEvent, memo, useCallback, useMemo, useState} from 'react';
+
+// Define the Status type
+type Status = 'pending' | 'ok' | 'error' | null;
+
+export interface FormData {
+    name: string;
+    email: string;
+    message: string;
+  };
+
+
+const FeedbackForm: FC = memo(() =>  {
+
+    const [status, setStatus] = useState<Status>(null);
+
+    const [error, setError] = useState<string | null>(null);
+
+    const inputClasses =
+    'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
+
+
+    const defaultData = useMemo(
+        () => ({
+        name: '',
+        email: '',
+        message: '',
+        }),
+        [],
+    );
+    
+    const [data, setData] = useState<FormData>(defaultData);
+    
+    const [formValues, updateFormValues] = useState<FormData>({
+        name: "",
+        email: "",
+        message: ""
+    });
+
+    const onChange = useCallback(
+        <T extends HTMLInputElement | HTMLTextAreaElement>(event: React.ChangeEvent<T>): void => {
+          const {name, value} = event.target;
+    
+          const fieldData: Partial<FormData> = {[name]: value};
+    
+          updateFormValues(formValues => ({...formValues, [name]: value}));
+          setData({...data, ...fieldData});
+        },
+        [data],
+      );
+
+    const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        try {
+            setStatus('pending');
+            setError(null);
+            const myForm = event.target as HTMLFormElement;
+            const formData = new FormData(myForm);
+            const res = await fetch('/__forms.html', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                body: new URLSearchParams(formData as any).toString()
+            });
+            if (res.status === 200) {
+                setStatus('ok');
+            } else {
+                setStatus('error');
+                setError(`${res.status} ${res.statusText}`);
+            }
+        } catch (e) {
+            setStatus('error');
+            setError(`${e}`);
+        }
+    };
+
+    return (
+        <form className="grid min-h-[320px] grid-cols-1 gap-y-4" data-netlify="true" method="POST" onSubmit={handleFormSubmit}>
+            <input 
+                className={inputClasses} 
+                id="name"
+                name="name" 
+                onChange={onChange} 
+                placeholder="Name" 
+                required 
+                type="text" 
+                value={formValues.name}
+            />
+            <input
+                autoComplete="email"
+                className={inputClasses}
+                id="email"
+                name="email"
+                onChange={onChange}
+                placeholder="Email"
+                required
+                type="email"
+                value={formValues.email}
+            />
+            <textarea
+                className={inputClasses}
+                id="message"
+                maxLength={250}
+                name="message"
+                onChange={onChange}
+                placeholder="Message"
+                required
+                rows={6}
+                value={formValues.message}
+            />
+            <button
+                aria-label="Submit contact form"
+                className="w-max rounded-full border-2 border-orange-600 bg-stone-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 focus:ring-offset-stone-800"
+                type="submit"
+                >
+                Send Message
+            </button>
+            {status === 'ok' && (
+                <div className="alert alert-success">
+                    Submitted!
+                </div>
+            )}
+            {status === 'error' && (
+                <div className="alert alert-error">
+                    {error}
+                </div>
+            )}
+        </form>
+  );
+});
+
+    FeedbackForm.displayName = 'FeedbackForm';
+    export default FeedbackForm;
+
+/*     return (
+        <div className="w-full md:max-w-md">
+            <Card title="Leave Feedback">
+                <form
+                    className="text-black flex flex-col gap-3 align-center"
+                    name="feedback"
+                    onSubmit={handleFormSubmit}
+                >
+                    <input name="form-name" type="hidden" value="feedback" />
+                    <input className="input input-bordered" name="name" placeholder="Name" required type="text" />
+                    <input className="input input-bordered" name="email" placeholder="Email (optional)" type="text" />
+                    <input className="input input-bordered" name="message" placeholder="Message" required type="text" />
+                    <button className="btn btn-primary" disabled={status === 'pending'} type="submit">
+                        Submit
+                    </button>
+                    {status === 'ok' && (
+                        <div className="alert alert-success">
+                            Submitted!
+                        </div>
+                    )}
+                    {status === 'error' && (
+                        <div className="alert alert-error">
+                            {error}
+                        </div>
+                    )}
+                </form>
+            </Card>
+        </div>
+    ); */
--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -10,7 +10,8 @@ import InstagramIcon from '../../Icon/InstagramIcon';
 import LinkedInIcon from '../../Icon/LinkedInIcon';
 import TwitterIcon from '../../Icon/TwitterIcon';
 import Section from '../../Layout/Section';
-import ContactForm from './ContactForm';
+// import ContactForm from './ContactForm';
+import FeedbackForm from './FeedbackForm';
 
 const ContactValueMap: Record<ContactType, ContactValue> = {
   [ContactType.Email]: {Icon: EnvelopeIcon, srLabel: 'Email'},
@@ -34,7 +35,7 @@ const Contact: FC = memo(() => {
         </div>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
           <div className="order-2 col-span-1 md:order-1 ">
-            <ContactForm />
+            <FeedbackForm />
           </div>
           <div className="order-1 col-span-1 flex flex-col gap-y-4 md:order-2">
             <p className="prose leading-6 text-neutral-300">{description}</p>
