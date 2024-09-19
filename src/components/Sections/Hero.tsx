aa2b451fcda1b21b71487f6de75cde6ee6547c3d
specialistchris <specialistchris@gmail.com>
2024-09-19 15:34:02 +0200

    return to backup

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -1,19 +1,18 @@
-import React, { FC, memo } from 'react';
+import {ChevronDownIcon} from '@heroicons/react/24/outline';
 import classNames from 'classnames';
 import Image from 'next/image';
+import {FC, memo} from 'react';
 
+import {heroData, SectionId} from '../../data/data';
 import Section from '../Layout/Section';
 import Socials from '../Socials';
-import { heroData } from '../../data/heroData'; // Assuming heroData is imported from a data file
-import { SectionId } from '../../data/sections'; // Assuming SectionId is imported from a data file
-import { ChevronDownIcon } from '@heroicons/react/solid'; // Assuming ChevronDownIcon is imported from heroicons
 
 const Hero: FC = memo(() => {
-  const { imageSrc, name, description, actions } = heroData;
+  const {imageSrc, name, description, actions} = heroData;
 
   return (
     <Section noPadding sectionId={SectionId.Hero}>
-      <div className="relative flex h-screen items-center justify-center">
+      <div className="relative flex h-screen w-full items-center justify-center">
         <Image
           alt={`${name}-image`}
           className="absolute z-0 h-full w-full object-cover"
@@ -21,23 +20,22 @@ const Hero: FC = memo(() => {
           priority
           src={imageSrc}
         />
-        <div className="z-10 max-w-screen-lg px-4 lg:px-0">
+        <div className="z-10  max-w-screen-lg px-4 lg:px-0">
           <div className="flex flex-col items-center gap-y-6 rounded-xl bg-gray-800/40 p-6 text-center shadow-lg backdrop-blur-sm">
             <h1 className="text-4xl font-bold text-white sm:text-5xl lg:text-7xl">{name}</h1>
-            <p className="text-white">{description}</p>
+            {description}
             <div className="flex gap-x-4 text-neutral-100">
               <Socials />
             </div>
             <div className="flex w-full justify-center gap-x-4">
-              {actions.map(({ href, text, primary, Icon }) => (
+              {actions.map(({href, text, primary, Icon}) => (
                 <a
                   className={classNames(
                     'flex gap-x-2 rounded-full border-2 bg-none px-4 py-2 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
                     primary ? 'border-orange-500 ring-orange-500' : 'border-white ring-white',
                   )}
                   href={href}
-                  key={text}
-                >
+                  key={text}>
                   {text}
                   {Icon && <Icon className="h-5 w-5 text-white sm:h-6 sm:w-6" />}
                 </a>
@@ -48,8 +46,7 @@ const Hero: FC = memo(() => {
         <div className="absolute inset-x-0 bottom-6 flex justify-center">
           <a
             className="rounded-full bg-white p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2"
-            href={`/#${SectionId.About}`}
-          >
+            href={`/#${SectionId.About}`}>
             <ChevronDownIcon className="h-5 w-5 bg-transparent sm:h-6 sm:w-6" />
           </a>
         </div>
