ed18fb57dd84fe21c6b5c110306c40984ca2b52e
specialistchris <72104201+specialistchris@users.noreply.github.com>
2026-06-04 14:47:17 +0200

    Add ALTCHA JSX type declaration

---
+++ src/types/altcha.d.ts
@@ -0,0 +1,15 @@
+import type React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'altcha-widget': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        challenge?: string;
+        name?: string;
+        ref?: React.Ref<HTMLElement>;
+      };
+    }
+  }
+}
+
+export {};
