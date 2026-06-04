ae12f0d868d61bbb4b64e90c7dfb99867ba2b301
specialistchris <72104201+specialistchris@users.noreply.github.com>
2026-06-04 15:43:41 +0200

    Update ALTCHA JSX prop type

--- src/types/altcha.d.ts
+++ src/types/altcha.d.ts
@@ -4,7 +4,7 @@ declare global {
   namespace JSX {
     interface IntrinsicElements {
       'altcha-widget': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-        challenge?: string;
+        challengeurl?: string;
         name?: string;
         ref?: React.Ref<HTMLElement>;
       };
